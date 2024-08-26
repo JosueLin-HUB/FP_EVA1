0ef473198828bb608922ad540106c7da53d8af47
JosueLin-HUB <L24550339@chihuahua2.tecnm.mx>
2024-08-26 16:51:59 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo"); //Cadena de texto
+        System.out.println(100); //Un numero entero
+        System.out.println('c'); //Un caracter
+        System.out.println(true);  //Valores ogicos
+        System.out.println(false);
+        System.out.println(1000.52); //Numero con fraccion
+        //Documentar todo papu :V
+        //Entender los errores y aprendermelos
+        
+              //Tipos de datos enteros
+        //int
+        //long
+        //byte
+        //short
+        
+              //Punto flotante
+        //double
+        //float
+        
+              //Tipos de texto
+        //String
+        //CARACTER
+        //char
+        //LÓGICOS (true false)
+        //boolean
+        
+              //Creacion y uso de variables
+        //DECLARACION
+          //Tipo de dato nombre de la variabl;
+          //que operaciones debe o puede hacer
+          //cuanta memoria va a utilizar
+        int edad; //Declarar una variable de tipo entero llamada edad
+        //double edad2;//No podemos tener dos variables con el mismo nombre
+        String nombre;
+        String apellidos;
+        double promedio;
+        //ASIGNACION
+          //Poner un valor a la variable
+        
+        edad = 18; //Estoy asignando el valor 18 dentro de la variable edad
+        System.out.println(edad); //println tambien imprime el contenido de las variable
+   
+       
+    }
+    
+}
