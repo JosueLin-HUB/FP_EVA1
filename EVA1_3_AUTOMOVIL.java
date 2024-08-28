9e1da752ca032ff737439d10f46cf46f9e278a4d
JosueLin-HUB <L24550339@chihuahua2.tecnm.mx>
2024-08-28 16:13:35 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_automovil;
+
+/**
+ *
+ * @author elfor
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    public static void main(String[] args) {
+             //DECLARACIONES
+        // Dejar las variables entendibles
+        String marca;
+        String modelo;
+        double precio;
+        int year;
+        //Trabajar en ingles para mejor al futuro
+        
+             //ASIGNACIONES
+        marca = "Ford";      //Esto le da un valor a la variable
+        modelo = "Mustang";  //Esto le da un valor a la variable
+        year = 1996;         //Esto le da un valor a la variable
+        precio = 999000.1;   //Esto le da un valor a la variable
+        System.out.println(marca);  //Esto imprime la variable 
+        System.out.println(modelo); //Esto imprime la variable
+        System.out.println(precio); //Esto imprime la variable
+        System.out.println(year);   //Esto imprime la variable
+    }
+}
