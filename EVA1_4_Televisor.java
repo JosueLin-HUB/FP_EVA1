8a4ff81832e4b262049a848157f7e59dee2562d1
JosueLin-HUB <L24550339@chihuahua2.tecnm.mx>
2024-08-28 16:13:58 -0600

    Add files via upload

---
+++ EVA1_4_Televisor.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_televisor;
+
+/**
+ *
+ * @author elfor
+ */
+public class EVA1_4_Televisor {
+
+    public static void main(String[] args) {
+    //DECLARACIONES
+    String marca;
+    String serie;
+    String pantalla;
+    int pulgadas;
+    double precio;
+    String estructura;
+    
+    //ASIGNAR
+    marca = "Samsung";
+    serie = "NEO";
+    pantalla = "Qled";
+    pulgadas = 60;
+    precio = 29999.99;
+    estructura = "Plana";
+    
+    //IMPRIMIR LAS VARIABLES
+    System.out.println(marca);
+    System.out.println(serie);
+    System.out.println(pantalla);
+    System.out.println(pulgadas);
+    System.out.println(precio);
+    System.out.println(estructura);
+    }
+}
