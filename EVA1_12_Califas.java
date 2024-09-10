0ebc0e8ad6b0642230d24de6980a2c8f7772123e
JosueLin-HUB <L24550339@chihuahua2.tecnm.mx>
2024-09-10 16:48:48 -0600

    Add files via upload

---
+++ EVA1_12_Califas.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_12_califas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author elfor
+ */
+public class EVA1_12_Califas {
+
+    public static void main(String[] args) {
+        int cali;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Dime tu caalificacion");
+        cali = captu.nextInt();
+        
+        if(cali>=70){
+        System.out.println("Acreditaste");
+        }else{
+        System.out.println("NA");}
+        }
+        
+        
+        
+        
+        
+        
+    }
+
---
+++ EVA1_13_Aguila_sol.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_13_aguila_sol;
+
+/**
+ *
+ * @author elfor
+ */
+public class EVA1_13_Aguila_sol {
+
+    public static void main(String[] args) {
+    double valor = Math.random();//esto genero un numero aleatoreo
+    //que esta entre  0 y .99999 
+    if(valor < 0.5){
+    System.out.println("Aguila");
+    }else{
+    System.out.println("Sello");
+    }
+                
+    }
+}
